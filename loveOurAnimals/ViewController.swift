da23c58aba580b576bca8e574e3fc84ea0fb4f47
webexpert.rvtech@gmail.com <webexpert.rvtech@gmail.com>
2020-02-18 11:21:06 +0530

    Initial Commit

---
+++ loveOurAnimals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loveOurAnimals
+//
+//  Created by swati sharma on 2/18/20.
+//  Copyright © 2020 swati sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ loveOurAnimalsTests/loveOurAnimalsTests.swift
@@ -0,0 +1,34 @@
+//
+//  loveOurAnimalsTests.swift
+//  loveOurAnimalsTests
+//
+//  Created by swati sharma on 2/18/20.
+//  Copyright © 2020 swati sharma. All rights reserved.
+//
+
+import XCTest
+@testable import loveOurAnimals
+
+class loveOurAnimalsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
